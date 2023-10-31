12a7c287f8a488893f944648a9d5cb5f54622287
Ray Lee <ray.lee@lyrasis.org>
2023-10-31 11:50:18 -0400

    Remove local storage. (#4)

--- test/specs/tokenHelpers.spec.js
+++ test/specs/tokenHelpers.spec.js
@@ -1,24 +1,7 @@
 import chai from 'chai';
-import { isLocalStorageAvailable } from '../../src/tokenHelpers';
 
 chai.should();
 
-describe('storageHelpers', () => {
-  const inNode = (typeof window === 'undefined');
+describe('tokenHelpers', () => {
 
-  describe('isLocalStorageAvailable()', () => {
-    if (inNode) {
-      context('in Node.js', () => {
-        it('should return false', () => {
-          isLocalStorageAvailable().should.equal(false);
-        });
-      });
-    } else {
-      context('in a browser', () => {
-        it('should return true', () => {
-          isLocalStorageAvailable().should.equal(true);
-        });
-      });
-    }
-  });
 });
